edd32cc32b2c57b284ea4f6cc9b7f9b1dc9e7ed0
Huszan <mateuszjacenty1@gmail.com>
2024-07-20 08:46:20 +0200

    Add app layout
    ## Add
    - template app layout
    ## Change
    - css files to scss
    - css imports to scss
    ## Dependency
    - sass
    - @uidotdev/usehooks

---
+++ src/components/content/Content.tsx
@@ -0,0 +1,10 @@
+import './Content.scss';
+
+export function Content() {
+
+    return (
+        <section className='content'>
+            
+        </section>
+    )
+}
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.scss';
+
+export function Header() {
+    
+    return (
+        <header>
+
+        </header>
+    )
+}
